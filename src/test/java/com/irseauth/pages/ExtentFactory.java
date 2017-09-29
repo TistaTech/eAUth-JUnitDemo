7d3ad438e1d154f1b61cfbf7351b339dbcfd105e
TistaTech <mmetoff@tistatech.com>
2017-09-29 16:08:50 -0400

    Update

--- src/test/java/com/irseauth/pages/ExtentFactory.java
+++ src/test/java/com/irseauth/pages/ExtentFactory.java
@@ -4,7 +4,7 @@ import com.relevantcodes.extentreports.ExtentReports;
 
 public class ExtentFactory {
 	public static ExtentReports getInstance() {
-		ExtentReports extent = new ExtentReports("./extentreports/ExtentReport.html", false);
+		ExtentReports extent = new ExtentReports("./target/ExtentReport.html", false);
 		extent
 			.addSystemInfo("Selenium Version", "2.5.0")
 		    .addSystemInfo("Chrome Version", "61");
